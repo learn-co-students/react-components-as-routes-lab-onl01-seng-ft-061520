3ef1b97a3e6813ced9167caba43ec5fd8887847e
Brycew30 <brycew30@gmail.com>
2020-10-23 10:15:46 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,19 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie, index) => (
+          <div key={index}>
+              <h4>Name: {movie.title}</h4>
+              <h4>Time: {movie.time}</h4>
+              <h4>Genres:</h4>
+              <ul>
+                  {movie.genres.map((genre, index) => (
+                      <li key={index}>{genre}</li>
+                  ))}
+              </ul>
+          </div>
+      ))}
     </div>
   );
 };
