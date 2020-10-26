9bec64854fbb06271630567f10dcc8e25264f960
Soundwood <j.benhogan@gmail.com>
2020-10-26 10:23:19 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,20 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((movie, index) => (
+        <div>
+          <h3>Movie: {movie.title}</h3>
+          <h3>Time: {movie.time}</h3>
+          <ul>
+            {movie.genres.map((genre, index) => (
+              <li>
+                {genre}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
