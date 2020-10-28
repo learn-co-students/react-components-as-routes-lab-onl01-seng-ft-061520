8da285762879a6754e929726266edba160da7a5b
fenglisa <lisafeng10@gmail.com>
2020-10-28 14:26:44 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,32 +1,24 @@
 import React from 'react';
 import { movies } from '../data';
 
-const MovieDetails = ({
-  title,
-  time,
-  genres,
-  metascore
-}) => {
+const Movies = () => {
   return (
     <div>
-      <h3>{title}</h3>
-      <p>{time}</p>
-      <ul>
-        {genres.map(genre => <li>{genre}</li>)}
-      </ul>
+      <h1>Movies Page</h1>
+      {movies.map((movie, index) => (
+          <div key={index}>
+              <h3>Name: {movie.title}</h3>
+              <p>Time: {movie.time}</p>
+              <p>Genres:</p>
+              <ul>
+                  {movie.genres.map((genre, index) => (
+                      <li key={index}>{genre}</li>
+                  ))}
+              </ul>
+          </div>
+      ))}
     </div>
   );
 };
-
-const Movies = ({ movieList }) => (
-    <div>
-      <h1>Movies Page</h1>
-      { movieList.map(MovieDetails) }
-    </div>
-  );
-
-  Movies.defaultProps = {
-    movieList: movies
-  };
 
 export default Movies;
