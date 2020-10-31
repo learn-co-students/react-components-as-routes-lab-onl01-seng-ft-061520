2426fa648ef82ee585e3fcd1c1ec6aaa5d2669c9
Swishyfishie2 <pclandreiuk@gmail.com>
2020-10-31 03:43:46 +0000

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,20 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>
+        Directors Page
+        </h1>
+      {directors.map(dir => {
+        return (
+          <div>
+            <h2>Name: {dir.name}</h2>
+            <p>Movies:</p>
+            <ul>
+              {dir.movies.map(m => <li>{m}</li>)}
+            </ul>
+          </div>
+        )
+      })}
     </div>
   );
 }
